c815ec94d1079e858940f12a2861f48bb7ee570b
Dunya Kirkali <dunyakirkali@gmail.com>
2016-07-09 19:13:34 +0200

    Clean up

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -2,13 +2,11 @@ class Admin::PagesController < ApplicationController
   before_action :set_page, only: [:show, :edit, :update, :destroy]
 
   # GET /admin/pages
-  # GET /admin/pages.json
   def index
     @pages = Page.all
   end
 
   # GET /admin/pages/1
-  # GET /admin/pages/1.json
   def show
   end
 
@@ -22,53 +20,37 @@ class Admin::PagesController < ApplicationController
   end
 
   # POST /admin/pages
-  # POST /admin/pages.json
   def create
     @page = Page.new(page_params)
-
-    respond_to do |format|
-      if @page.save
-        format.html { redirect_to [:admin, @page], notice: 'Page was successfully created.' }
-        format.json { render :show, status: :created, location: [:admin, @page] }
-      else
-        format.html { render :new }
-        format.json { render json: @page.errors, status: :unprocessable_entity }
-      end
+    if @page.save
+      redirect_to [:admin, @page], notice: 'Page was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /admin/pages/1
-  # PATCH/PUT /admin/pages/1.json
   def update
-    respond_to do |format|
-      if @page.update(page_params)
-        format.html { redirect_to [:admin, @page], notice: 'Page was successfully updated.' }
-        format.json { render :show, status: :ok, location: [:admin, @page] }
-      else
-        format.html { render :edit }
-        format.json { render json: @page.errors, status: :unprocessable_entity }
-      end
+    if @page.update(page_params)
+      redirect_to [:admin, @page], notice: 'Page was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /admin/pages/1
-  # DELETE /admin/pages/1.json
   def destroy
     @page.destroy
-    respond_to do |format|
-      format.html { redirect_to admin_pages_url, notice: 'Page was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to admin_pages_url, notice: 'Page was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_page
-      @page = Page.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def page_params
-      params.require(:page).permit(:title, :content)
-    end
+  def set_page
+    @page = Page.find(params[:id])
+  end
+
+  def page_params
+    params.require(:page).permit(:title, :content)
+  end
 end
--- app/views/pages/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @page, :id, :title, :content, :created_at, :updated_at
